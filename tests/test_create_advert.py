6979723ba3999f1b988c0f14aafe52735106c85d
Ruslan Churekov <rchurekov@skala-r.ru>
2025-05-19 22:03:44 +0300

    Sprint_5 v.3

--- tests/test_create_advert.py
+++ tests/test_create_advert.py
@@ -5,53 +5,54 @@ from data import BASE_URL, PROFILE_URL, EMAIL, PASSWORD
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_create_advert_unauthorized_user(driver: WebDriver):
-    driver.get(BASE_URL)
+class CreateAdvert:
+    def test_create_advert_unauthorized_user(self, driver: WebDriver):
+        driver.get(BASE_URL)
 
-    driver.find_element(*CreateAdvert.NEW_ADVERT).click()
+        driver.find_element(*CreateAdvert.NEW_ADVERT).click()
 
-    WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located(CreateAdvert.MODAL_WINDOW)
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(CreateAdvert.MODAL_WINDOW)
+        )
+        assert driver.find_element(*CreateAdvert.MODAL_WINDOW).is_displayed()
+        assert driver.find_element(*CreateAdvert.MODAL_WINDOW_TITLE).text == "Чтобы разместить объявление, авторизуйтесь"
+        
+
+    def test_create_advert_authorized_user_success(self, driver: WebDriver):
+        driver.get(BASE_URL)
+
+        driver.find_element(*AutorisationLocators.LOGIN_BUTTON).click()
+
+        WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((AutorisationLocators.EMAIL_INPUT))
+    ).send_keys(EMAIL)
+        driver.find_element(*AutorisationLocators.PASSWORD_INPUT).send_keys(PASSWORD)
+        driver.find_element(*AutorisationLocators.ENTER_BUTTON).click()
+
+        WebDriverWait(driver, 10).until(
+        EC.invisibility_of_element_located(CreateAdvert.MODAL_WINDOW)
     )
-    assert driver.find_element(*CreateAdvert.MODAL_WINDOW).is_displayed()
-    assert driver.find_element(*CreateAdvert.MODAL_WINDOW_TITLE).text == "Чтобы разместить объявление, авторизуйтесь"
-    
 
-def test_create_advert_authorized_user_success(driver: WebDriver):
-    driver.get(BASE_URL)
+        WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(CreateAdvert.NEW_ADVERT)
+    ).click()
 
-    driver.find_element(*AutorisationLocators.LOGIN_BUTTON).click()
+        driver.find_element(*CreateAdvert.NAME_INPUT).send_keys('Iphone 15')
+        driver.find_element(*CreateAdvert.PRODUCT_DESCRIPTION).send_keys('iPhone 15 128 ГБ Серый')
+        driver.find_element(*CreateAdvert.PRICE_INPUT).send_keys('50000')
 
-    WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((AutorisationLocators.EMAIL_INPUT))
-).send_keys(EMAIL)
-    driver.find_element(*AutorisationLocators.PASSWORD_INPUT).send_keys(PASSWORD)
-    driver.find_element(*AutorisationLocators.ENTER_BUTTON).click()
+        driver.find_element(*CreateAdvert.CATEGORY_INPUT).click()
+        driver.find_element(*CreateAdvert.TECHNOLOGY_BUTTON).click()
 
-    WebDriverWait(driver, 10).until(
-    EC.invisibility_of_element_located(CreateAdvert.MODAL_WINDOW)
-)
+        driver.find_element(*CreateAdvert.CITY_INPUT).click()
+        driver.find_element(*CreateAdvert.SPB_BUTTON).click()
 
-    WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable(CreateAdvert.NEW_ADVERT)
-).click()
+        driver.find_element(*CreateAdvert.RABIO_BUTTON_BU).click()
+        driver.find_element(*CreateAdvert.TO_PUBLISH).click()
 
-    driver.find_element(*CreateAdvert.NAME_INPUT).send_keys('Iphone 15')
-    driver.find_element(*CreateAdvert.PRODUCT_DESCRIPTION).send_keys('iPhone 15 128 ГБ Серый')
-    driver.find_element(*CreateAdvert.PRICE_INPUT).send_keys('50000')
+        driver.get(PROFILE_URL)
 
-    driver.find_element(*CreateAdvert.CATEGORY_INPUT).click()
-    driver.find_element(*CreateAdvert.TECHNOLOGY_BUTTON).click()
-
-    driver.find_element(*CreateAdvert.CITY_INPUT).click()
-    driver.find_element(*CreateAdvert.SPB_BUTTON).click()
-
-    driver.find_element(*CreateAdvert.RABIO_BUTTON_BU).click()
-    driver.find_element(*CreateAdvert.TO_PUBLISH).click()
-
-    driver.get(PROFILE_URL)
-
-    WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located(CreateAdvert.CARD_SEARCH)
-)
-    assert driver.find_element(*CreateAdvert.CARD_SEARCH).text == "Iphone 15"
+        WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located(CreateAdvert.CARD_SEARCH)
+    )
+        assert driver.find_element(*CreateAdvert.CARD_SEARCH).text == "Iphone 15"
